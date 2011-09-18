a382ba1a4cd3f9b4d24428fbfa2d782a94d25ed9
Ed James <ed.james.email@gmail.com>
2011-09-18 23:52:20 +0100

    removed old crap; updated gemspec

--- lib/pivot_table.rb
+++ lib/pivot_table.rb
@@ -1,9 +1,6 @@
 $: << File.dirname(__FILE__)
-require "pivot_table/calculator"
-require "pivot_table/reader"
 require "pivot_table/grid"
-require "pivot_table/table"
 
 module PivotTable
-  VERSION = "0.0.3"
+  VERSION = "0.1.0"
 end
